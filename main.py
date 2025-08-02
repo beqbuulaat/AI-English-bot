8aa2ed760a77cc71e3beb28f2230df4fb02e9efd
beqbuulaat <beqbuu1aat@gmail.com>
2025-08-02 15:11:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -61,3 +61,7 @@ def set_webhook():
 # Запуск Flask
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    if __name__ == '__main__':
+    bot.remove_webhook()
+    bot.set_webhook(url=f"{WEBHOOK_URL}/webhook")
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
